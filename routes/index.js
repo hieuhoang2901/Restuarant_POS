b113072cdbfbafe8eb099fcc98867331f12babfc
BuiXuanThong <akirakuma5@gmail.com>
2021-11-28 18:47:48 +0700

    add searchpage, fix icon href

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var Cart = require('../app/middleware/cart')
 var connection = require('../config/db/db');
 /* GET home page. */
 router.get('/', homeController.index);
+// router.post('/', site_controller.redirect_to_search);
+router.get('/search/:data', homeController.search);
 var foodServices = require('../app/services/foodServices');
 
 
