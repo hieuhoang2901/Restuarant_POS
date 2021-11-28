b0bb3d4609330d7d02b852071082a1d29c5c3828
KuangHcmUT <quang.nguyenqnvnbk@hcmut.edu.vn>
2021-11-28 09:58:04 +0700

    add header and footer

--- app/controller/manageFeedbackController.js
+++ app/controller/manageFeedbackController.js
@@ -5,7 +5,7 @@ class manageFeedbackController {
     listFeedback(req, res, next) {
         manageFeedbackServices.getAllFeedback().then(async function(rows) {
             try {
-                res.render('manageFeedback', { layout: 'index', rows });
+                res.render('manageFeedback', { layout: 'index', rows, user: req.user });
             } catch (err) {
                 console.log(err);
             }
--- app/controller/manageUserController.js
+++ app/controller/manageUserController.js
@@ -5,33 +5,33 @@ class manageUserController {
     listUser(req, res, next) {
         manageUserServices.getAllUser().then(async function(rows) {
             try {
-                res.render('manageUser', { layout: 'index', rows });
+                res.render('manageUser', { layout: 'main', rows, user: req.user });
             } catch (err) {
                 console.log(err);
             }
         });
     }
     addNewUser(req, res, next) {
-        //create a new user
-        let newUser = {
-            name: req.body.userName,
-            email: req.body.userEmail,
-            password: req.body.userPassword,
-            address: req.body.userAddress,
-            phoneNumber: req.body.userPhone,
-            userRole: req.body.userRole
-        };
-        console.log(newUser)
+            //create a new user
+            let newUser = {
+                name: req.body.userName,
+                email: req.body.userEmail,
+                password: req.body.userPassword,
+                address: req.body.userAddress,
+                phoneNumber: req.body.userPhone,
+                userRole: req.body.userRole
+            };
+            console.log(newUser)
 
-        manageUserServices.addNewUser(newUser).then(async function(rows) {
-            try {
-                res.redirect('/manageUser');
-            } catch (err) {
-                console.log(err)
-            }
-        });
-    }
-    // get information of user to update
+            manageUserServices.addNewUser(newUser).then(async function(rows) {
+                try {
+                    res.redirect('/manageUser');
+                } catch (err) {
+                    console.log(err)
+                }
+            });
+        }
+        // get information of user to update
     getUser(req, res, next) {
         var id = parseInt(req.params.id);
         manageUserServices.getUser(id).then(async function(rows) {
